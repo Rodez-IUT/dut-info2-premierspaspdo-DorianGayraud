3011a881e4942e00409256eb56d8e3fb83b8d850
DorianGayraud <dorian.gayraud@iut-rodez.fr>
2019-11-06 10:29:07 +0100

    Recuperation ligne users et classement par ordre SANS presentation

---
+++ all_users.php
@@ -0,0 +1,44 @@
+<!doctype HTML>
+
+<html>
+<head>
+	<title>User dans ordre alphabétique</title>
+	<meta charset="utf-8">
+</head>
+
+<body>
+
+	<!-- Connexion à la base de donnée -->
+	<?php
+        $host = 'localhost';
+		$db   = 'my_activities';
+		$user = 'root';
+		$pass = 'root';
+		$charset = 'utf8mb4';
+		$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+		$options = [
+  		  PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+  		  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+  		  PDO::ATTR_EMULATE_PREPARES   => false,
+		];
+		try {
+		     $pdo = new PDO($dsn, $user, $pass, $options);
+		} catch (PDOException $e) {
+   		  throw new PDOException($e->getMessage(), (int)$e->getCode());
+		}
+	?>
+    
+    <!-- SCript pjp pour afficher dans l'ordre alphabétique sur le "username" -->
+    <?php 
+        $stmt = $pdo->query('SELECT * FROM users ORDER BY username');
+        while ($row = $stmt->fetch()) {
+        	/* on affiche toutes les lignes */
+        	foreach ($row as $key => $value) {
+        		echo $value;
+        	}
+        	echo "<br />";
+        }
+    ?>
+</body>
+
+</html>
